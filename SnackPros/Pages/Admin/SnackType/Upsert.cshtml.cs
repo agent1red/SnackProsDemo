29fc6990d42310d85985548054900fc3a1bc3a3f
Kevin Hudson <darklen14@gmail.com>
2020-10-02 14:57:53 -0400

    ++Upsert and js file for SnackType and

---
+++ SnackPros/Pages/Admin/SnackType/Upsert.cshtml.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SnackPros.DataAccess.Data.Repository.IRepository;
+
+namespace SnackPros.Pages.Admin.SnackType
+{
+    public class UpsertModel : PageModel
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UpsertModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [BindProperty]
+        public Models.SnackType SnackTypeObj { get; set; }
+
+
+        public IActionResult OnGet(int? id)
+        {
+            SnackTypeObj = new Models.SnackType();
+            if (id != null)
+            {
+                SnackTypeObj = _unitOfWork.SnackType.GetFirstOrDefault(u => u.Id == id);
+                if (SnackTypeObj == null)
+                {
+                    return NotFound();
+                }
+            }
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            if (SnackTypeObj.Id == 0)
+            {
+                _unitOfWork.SnackType.Add(SnackTypeObj);
+            }
+            else
+            {
+                _unitOfWork.SnackType.Update(SnackTypeObj);
+            }
+            _unitOfWork.Save();
+            return RedirectToPage("./Index");
+        }
+
+
+
+    }
+}

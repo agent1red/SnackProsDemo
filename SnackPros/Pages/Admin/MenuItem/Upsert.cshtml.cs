b114865deaed88b5050451c583bb8cff94ded47c
Kevin Hudson <darklen14@gmail.com>
2020-10-03 18:43:25 -0400

    ++MenuItem Upsert Page and CRUD Func

---
+++ SnackPros/Pages/Admin/MenuItem/Upsert.cshtml.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SnackPros.DataAccess.Data.Repository.IRepository;
+using SnackPros.Models.ViewModels;
+
+namespace SnackPros.Pages.Admin.MenuItem
+{
+    public class UpsertModel : PageModel
+    {
+            private readonly IUnitOfWork _unitOfWork;
+            private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public UpsertModel(IUnitOfWork unitOfWork, IWebHostEnvironment hostingEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        // Object of MenuItem property from view model
+        [BindProperty]
+        public MenuItemVM MenuItemObj { get; set; }
+
+        //populate the viewmodel dropdown for Categorylist  and Snack list
+        public IActionResult OnGet(int? id)
+        {
+            MenuItemObj = new MenuItemVM
+            {
+                CategoryList = _unitOfWork.Category.GetCategoryListForDropDown(),
+                SnackTypeList = _unitOfWork.SnackType.GetSnackTypeListForDropDown(),
+                MenuItem = new Models.MenuItem()
+            };
+            if (id != null)
+            {
+                MenuItemObj.MenuItem = _unitOfWork.MenuItem.GetFirstOrDefault(u => u.Id == id);
+                if (MenuItemObj == null)
+                {
+                    return NotFound();
+                }
+            }
+            return Page();
+        }
+
+
+        //Save the image the user uploads to menuItems image folder
+        public IActionResult OnPost()
+        {
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            if (MenuItemObj.MenuItem.Id == 0)
+            {
+                //save file as Guid
+                string fileName = Guid.NewGuid().ToString();
+                //Find the path for uploads 
+                var uploads = Path.Combine(webRootPath, @"images\menuItems");
+                var extension = Path.GetExtension(files[0].FileName);
+
+                using (var fileStream = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                {
+                    files[0].CopyTo(fileStream);
+                }
+                MenuItemObj.MenuItem.Image = @"\images\menuItems\" + fileName + extension;
+
+                _unitOfWork.MenuItem.Add(MenuItemObj.MenuItem);
+            }
+            else
+            {
+                //EDIT menu item retrieved from database
+                var objFromDb = _unitOfWork.MenuItem.Get(MenuItemObj.MenuItem.Id);
+                if (files.Count > 0)
+                {
+                    //New file wasn't uploaded
+                
+                    string fileName = Guid.NewGuid().ToString();
+                    var uploads = Path.Combine(webRootPath, @"images\menuItems");
+                    var extension = Path.GetExtension(files[0].FileName);
+
+                    var imagePath = Path.Combine(webRootPath, objFromDb.Image.TrimStart('\\'));
+                    if (System.IO.File.Exists(imagePath))
+                    {
+                        System.IO.File.Delete(imagePath);
+                    }
+
+
+                    using (var fileStream = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                    {
+                        files[0].CopyTo(fileStream);
+                    }
+                    MenuItemObj.MenuItem.Image = @"\images\menuItems\" + fileName + extension;
+                    
+                }
+                else
+                {
+                    MenuItemObj.MenuItem.Image = objFromDb.Image;
+                }
+
+
+                _unitOfWork.MenuItem.Update(MenuItemObj.MenuItem);
+            }
+            _unitOfWork.Save();
+            return RedirectToPage("./Index");
+        }
+
+
+
+    }
+}

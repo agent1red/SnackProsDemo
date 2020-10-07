1ee7895ec810ceda5df39025a24fbca7feffaab0
Kevin Hudson <darklen14@gmail.com>
2020-10-07 18:58:26 -0400

    Laoded the shopping cart Counter session

--- SnackPros/Pages/Customer/Home/Index.cshtml.cs
+++ SnackPros/Pages/Customer/Home/Index.cshtml.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SnackPros.DataAccess.Data.Repository.IRepository;
 using SnackPros.Models;
+using SnackPros.Utility;
 
 namespace SnackPros.Pages.Customer.Home
 {
@@ -24,6 +27,18 @@ namespace SnackPros.Pages.Customer.Home
 
         public void OnGet()
         {
+
+            // Retrieve the user id of the logged in user to load session for shopping cart 
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (claim != null)
+            {
+                int ShoppingCartCount = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).ToList().Count;
+                // assign to HttpContext 
+                HttpContext.Session.SetInt32(SD.ShoppingCart, ShoppingCartCount);
+            }
+
             MenuItemList = _unitOfWork.MenuItem.GetAll(null, null, "Category,SnackType");
             // ordered definition here for Category items by ascending order
             CategoryList = _unitOfWork.Category.GetAll(null, q => q.OrderBy(c => c.DisplayOrder), null);

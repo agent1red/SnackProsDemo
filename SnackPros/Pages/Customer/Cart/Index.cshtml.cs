04c50356fae33ceec3cfa332318aee03106ae033
Kevin Hudson <darklen14@gmail.com>
2020-10-08 13:26:23 -0400

    updated shopping cart

--- SnackPros/Pages/Customer/Cart/Index.cshtml.cs
+++ SnackPros/Pages/Customer/Cart/Index.cshtml.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SnackPros.DataAccess.Data.Repository.IRepository;
 using SnackPros.Models;
+using SnackPros.Utility;
 
 namespace SnackPros.Pages.Customer.Cart
 {
@@ -52,6 +55,51 @@ namespace SnackPros.Pages.Customer.Cart
                 OrderDetailsCartVM.OrderHeader.OrderTotal += (cartList.MenuItem.Price * cartList.Count);
             }
 
+        }
+
+        public  IActionResult OnPostPlus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(c => c.Id == cartId);
+            _unitOfWork.ShoppingCart.IncrementCount(cart, 1);
+            _unitOfWork.Save();
+            return RedirectToPage("/Customer/Cart/Index");
+        }
+        public  IActionResult OnPostMinus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(c => c.Id == cartId);
+
+            if (cart.Count == 1)
+            {
+                _unitOfWork.ShoppingCart.Remove(cart);
+                _unitOfWork.Save();
+                var cnt = _unitOfWork.ShoppingCart
+                    .GetAll(u => u.ApplicationUserId == cart.ApplicationUserId)
+                    .ToList()
+                    .Count;
+                HttpContext.Session.SetInt32(SD.ShoppingCart, cnt);
+
+            }
+            else
+            {
+            _unitOfWork.ShoppingCart.DecrementCount(cart, 1);
+            _unitOfWork.Save();
+
+            }
+            return RedirectToPage("/Customer/Cart/Index");
+        }
+
+        public IActionResult OnPostRemove(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(c => c.Id == cartId);
+            _unitOfWork.ShoppingCart.Remove(cart);
+            _unitOfWork.Save();
+
+            var cnt = _unitOfWork.ShoppingCart
+                    .GetAll(u => u.ApplicationUserId == cart.ApplicationUserId)
+                    .ToList()
+                    .Count;
+            HttpContext.Session.SetInt32(SD.ShoppingCart, cnt);
+            return RedirectToPage("/Customer/Cart/Index");
         }
     }
 }

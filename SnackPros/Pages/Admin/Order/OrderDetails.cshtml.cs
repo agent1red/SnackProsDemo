89461e7ae5cf1695fd5c40d627272db24b8854d4
Kevin Hudson <darklen14@gmail.com>
2020-10-11 15:02:16 -0400

    Updated OrderDetails for refund and cancel

--- SnackPros/Pages/Admin/Order/OrderDetails.cshtml.cs
+++ SnackPros/Pages/Admin/Order/OrderDetails.cshtml.cs
@@ -5,7 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SnackPros.DataAccess.Data.Repository.IRepository;
+using SnackPros.Models;
 using SnackPros.Models.ViewModels;
+using SnackPros.Utility;
+using Stripe;
 
 namespace SnackPros.Pages.Admin.Order
 {
@@ -29,6 +32,42 @@ namespace SnackPros.Pages.Admin.Order
             };
 
             OrderDetailsVM.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser.GetFirstOrDefault(u => u.Id == OrderDetailsVM.OrderHeader.UserId);
+        }
+
+        public IActionResult OnPostOrderConfirm(int orderId)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(o => o.Id == orderId);
+            orderHeader.Status = SD.StatusCompleted;
+            _unitOfWork.Save();
+            return RedirectToPage("OrderList");
+        }
+
+        public IActionResult OnPostOrderCancel(int orderId)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(o => o.Id == orderId);
+            orderHeader.Status = SD.StatusCancelled;
+            _unitOfWork.Save();
+            return RedirectToPage("OrderList");
+        }
+
+        public IActionResult OnPostOrderRefund(int orderId)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(o => o.Id == orderId);
+
+            // refund the amount 
+            var options = new RefundCreateOptions
+            {
+                Amount = Convert.ToInt32(orderHeader.OrderTotal * 100),
+                Reason = RefundReasons.RequestedByCustomer,
+                Charge = orderHeader.TransactionId
+            };
+            var service = new RefundService();
+            Refund refund = service.Create(options);
+
+            //change the status to refund
+            orderHeader.Status = SD.StatusRefunded;
+            _unitOfWork.Save();
+            return RedirectToPage("OrderList");
         }
     }
 }

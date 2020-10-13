49a7f2d752c4e5d5e7c7436a57852743a21d6e15
Kevin Hudson <darklen14@gmail.com>
2020-10-13 02:38:26 -0400

    Updated timer function for ManageOrder page counter

---
+++ SnackPros/wwwroot/js/countTimer.js
@@ -0,0 +1,30 @@
+﻿setInterval(countdown, 60000);
+
+
+
+//function countdown() {
+//    var daysLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+//    var hoursLeft = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
+//    var minutesLeft = Math.floor((timeLeft / 1000) / 60 % 60);
+
+//    const timeLeftText = `${daysLeft} days ${hoursLeft} hours ${minutesLeft} minutes.`;
+//    document.getElementById("timer").innerHTML = timeLeftText;
+//}
+//countdown();
+
+function countdown() {
+    var timeLeft = pickUpTime - new Date();
+    var daysLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    var hoursLeft = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
+    var minutesLeft = Math.floor((timeLeft / 1000) / 60 % 60);
+
+    const timeLeftText = `${daysLeft} days ${hoursLeft} hours ${minutesLeft} minutes.`;
+    x = document.getElementsByClassName("timer");
+    for (var i = 0; i < x.length; i++) {
+        x[i].innerHTML = timeLeftText;
+    }
+   // document.getElementsByClassName("timer")[0].innerHTML = timeLeftText;
+  
+};
+
+countdown();

eff49104ef07076e979fb6f608e6217b43f324c5
Kevin Hudson <darklen14@gmail.com>
2020-10-13 20:39:51 -0400

    updated countTimer function

--- SnackPros/wwwroot/js/countTimer.js
+++ SnackPros/wwwroot/js/countTimer.js
@@ -1,30 +1,31 @@
-﻿setInterval(countdown, 60000);
+﻿
 
-
-
-//function countdown() {
-//    var daysLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-//    var hoursLeft = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
-//    var minutesLeft = Math.floor((timeLeft / 1000) / 60 % 60);
-
-//    const timeLeftText = `${daysLeft} days ${hoursLeft} hours ${minutesLeft} minutes.`;
-//    document.getElementById("timer").innerHTML = timeLeftText;
-//}
-//countdown();
-
-function countdown() {
-    var timeLeft = pickUpTime - new Date();
-    var daysLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-    var hoursLeft = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
-    var minutesLeft = Math.floor((timeLeft / 1000) / 60 % 60);
-
-    const timeLeftText = `${daysLeft} days ${hoursLeft} hours ${minutesLeft} minutes.`;
-    x = document.getElementsByClassName("timer");
-    for (var i = 0; i < x.length; i++) {
-        x[i].innerHTML = timeLeftText;
-    }
-   // document.getElementsByClassName("timer")[0].innerHTML = timeLeftText;
-  
-};
-
+var exp = document.getElementsByClassName('exp1');
+x = document.getElementsByClassName("timer");
 countdown();
+function countdown() {
+    var now = new Date();
+    for (i = 0; i < exp.length; i++) {
+        var e = new Date(exp[i].innerHTML);
+        var distance = e - now;
+        var timeDiff = e.getTime() - now.getTime();
+        var seconds = Math.floor(timeDiff / 1000);
+        var minutes = Math.floor(seconds / 60);
+        var hours = Math.floor(minutes / 60);
+        var days = Math.floor(hours / 24);
+        hours %= 24;
+        minutes %= 60;
+        seconds %= 60;
+
+        if (distance < 0) {
+            clearInterval(countdown);
+            x[i].className = "col-12 timer text-center text-wrap font-weight-bold mb-4"
+            x[i].innerHTML = "<span style='color: red;'>Time Expired </span>"
+
+        } else {
+            x[i].innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+        }
+    }
+
+}
+setInterval(function () { countdown();; }, 1000);
